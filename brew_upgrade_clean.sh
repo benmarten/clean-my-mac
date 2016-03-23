86c71443e1b23cbfa19ffc83afe9f008a98d469a
Ben Marten <ben.marten@me.com>
2016-03-23 13:26:19 -0700

    added node upgrade script

--- brew_upgrade_clean.sh
+++ brew_upgrade_clean.sh
@@ -5,4 +5,5 @@ brew cask update
 brew upgrade
 brew cleanup
 brew cask cleanup
+mkdir -p '/Library/Caches/Homebrew/Casks'
 brew doctor
---
+++ node.sh
@@ -0,0 +1,26 @@
+#!/bin/fish
+
+set NODE_VERSION_REMOTE (nvm version-remote node)
+set NODE_VERSION (nvm version node)
+
+# echo $NODE_VERSION
+# echo $NODE_VERSION_REMOTE
+
+if [ $NODE_VERSION_REMOTE = $NODE_VERSION ] 
+	echo "Latest node version is installed! ($NODE_VERSION)"
+	exit 1
+end
+
+echo "Upgrading node $NODE_VERSION -> $NODE_VERSION_REMOTE ..."
+
+nvm install node
+# nvm use node
+nvm alias default $NODE_VERSION_REMOTE
+echo "Reinstalling global modules ..."
+nvm reinstall-packages $NODE_VERSION
+nvm uninstall $NODE_VERSION
+echo "Done!"
+
+# # Temp fix for nvm linking issue
+ln -s ~/.nvm/versions/node/(node -v)/bin/node /usr/local/bin/ 
+ln -s ~/.nvm/versions/node/(node -v)/bin/npm /usr/local/bin/
